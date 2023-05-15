dd8e21ea44913a091b5f64748e13d60691c1b7d0
seunggabi <seunggabi@naver.com>
2023-05-16 00:03:15 +0900

    (#1) refactor: change class -> method

--- seunggabi_core_python/util/config_util.py
+++ seunggabi_core_python/util/config_util.py
@@ -5,38 +5,36 @@ from seunggabi_core_python.const import DEFAULT, CREDENTIALS
 from seunggabi_core_python.exception.bad_request import BadRequest
 
 
-class Config:
-    @staticmethod
-    def home():
-        return os.path.expanduser("~")
+def home():
+    return os.path.expanduser("~")
 
-    @staticmethod
-    def path(
-        group: str = None,
-        context: str = None,
-    ):
-        return f".{group}/{context}"
 
-    @staticmethod
-    def get(
-        group: str = None,
-        context: str = None,
-        profile: str = None,
-    ) -> dict:
-        if group is None or context is None:
-            raise BadRequest()
+def path(
+    group: str = None,
+    context: str = None,
+):
+    return f".{group}/{context}"
 
-        profile = profile or DEFAULT
 
-        config = ConfigParser()
-        config.read(f"{Config.home()}/{Config.path(group, context)}")
+def get(
+    group: str = None,
+    context: str = None,
+    profile: str = None,
+) -> dict:
+    if group is None or context is None:
+        raise BadRequest()
 
-        return config._sections[profile]
+    profile = profile or DEFAULT
+
+    config = ConfigParser()
+    config.read(f"{home()}/{path(group, context)}")
+
+    return config._sections[profile]
 
 
 if __name__ == '__main__':
     print(
-        Config.get(
+        get(
             group="upbit",
             context=CREDENTIALS,
         )
