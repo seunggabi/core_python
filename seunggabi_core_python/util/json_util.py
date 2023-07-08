4a63387cfccd67210291671f7284734d00df1992
seunggabi <seunggabi@naver.com>
2023-07-08 23:11:47 +0900

    feat: add to dict in json_util

--- seunggabi_core_python/util/json_util.py
+++ seunggabi_core_python/util/json_util.py
@@ -1,10 +1,14 @@
 import json
 
 
-def pretty(obj) -> str:
+def pretty(obj: dict) -> str:
     return json.dumps(
         obj,
         ensure_ascii=False,
         sort_keys=True,
         indent=4
     )
+
+
+def to_dict(s: str) -> dict:
+    return json.load(s)
