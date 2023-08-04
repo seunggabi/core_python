a15efd6c7c7c72fbe4fa4f8822d05e687604e2b8
seunggabi <seunggabi@naver.com>
2023-08-04 18:12:39 +0900

    feat: request_util.get retries

---
+++ seunggabi_core_python/exception/not_ok.py
@@ -0,0 +1,6 @@
+class NotOk(Exception):
+    def __init__(
+        self,
+        msg: str = ""
+    ):
+        super().__init__(f"[!200 NOT_OK] {msg}")
--- seunggabi_core_python/util/request_util.py
+++ seunggabi_core_python/util/request_util.py
@@ -1,16 +1,25 @@
 import requests
 
 from typing import Union
+from seunggabi_core_python.exception.not_ok import NotOk
 
 
 def get(
     url: str,
-    params: Union[dict, None] = None
+    params: Union[dict, None] = None,
+    retry: int = 3,
 ) -> dict:
     params = params or {}
 
     response = requests.get(url, params=params)
-    try:
-        return response.json()
-    except:
-        return response.text
+    if response.status_code == 200:
+        try:
+            return response.json()
+        except:
+            return response.text
+    else:
+        retry -= 1
+        if retry == 0:
+            raise NotOk(response.text)
+
+        return get(url, params, retry)
