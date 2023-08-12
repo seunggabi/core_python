93887fddd781e467aaf4450380ad857c8e8b51ab
seunggabi <seunggabi@naver.com>
2023-08-12 11:42:55 +0900

    (#21) feat: safe_get expand keys

--- seunggabi_core_python/util/obj_util.py
+++ seunggabi_core_python/util/obj_util.py
@@ -3,10 +3,24 @@ def safe_get(
     key: str,
     default=None,
 ):
-    return obj[key] if key in obj else default
+    k = key.split(".")
+
+    try:
+        for i in k:
+            obj = obj[i]
+        return obj if obj is not None else default
+    except:
+        return default
 
 
 if __name__ == '__main__':
-    obj = {"a": 1}
+    obj = {
+        "a": 1,
+        "z": {
+            "b": 0
+        }
+    }
     print(safe_get(obj, "a"))
     print(safe_get(obj, "b"))
+    print(safe_get(obj, "b", 3))
+    print(safe_get(obj, "z.b"))
