a17b3e9d910028d2ca8207e02f312ce28df7d949
seunggabi <seunggabi@naver.com>
2023-08-19 23:31:05 +0900

    feat: add headers in request_util

--- seunggabi_core_python/util/request_util.py
+++ seunggabi_core_python/util/request_util.py
@@ -8,10 +8,19 @@ def get(
     url: str,
     params: Union[dict, None] = None,
     retry: int = 3,
+    headers=None,
 ) -> dict:
     params = params or {}
+    headers = headers or {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+    }
 
-    response = requests.get(url, params=params)
+    response = requests.get(
+        url,
+        params=params,
+        headers=headers,
+    )
+
     if response.status_code == 200:
         try:
             return response.json()
