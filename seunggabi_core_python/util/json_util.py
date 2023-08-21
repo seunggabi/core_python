00afddd7fdfd4770f1d0990a1f36f21ea29f88e0
seunggabi <seunggabi@naver.com>
2023-08-21 22:26:23 +0900

    fix: single quote -> to_dict 2

--- seunggabi_core_python/util/json_util.py
+++ seunggabi_core_python/util/json_util.py
@@ -4,6 +4,9 @@ from typing import Union
 
 def pretty(obj: Union[dict, str]) -> str:
     try:
+        if isinstance(obj, str):
+            obj = to_dict(obj)
+
         return json.dumps(
             obj,
             ensure_ascii=False,
