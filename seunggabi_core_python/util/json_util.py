d050a6094912a22b76d81383c12a0240517f8156
seunggabi <seunggabi@naver.com>
2023-08-21 21:50:22 +0900

    feat: str -> dict

--- seunggabi_core_python/util/json_util.py
+++ seunggabi_core_python/util/json_util.py
@@ -1,8 +1,12 @@
 import json
+from typing import Union
 
 
-def pretty(obj: dict) -> str:
+def pretty(obj: Union[dict, str]) -> str:
     try:
+        if isinstance(obj, str):
+            obj = to_dict(obj)
+
         return json.dumps(
             obj,
             ensure_ascii=False,
