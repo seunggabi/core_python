c3082b6d058eda4f6aa3ff4a89da782ab23354f9
seunggabi <seunggabi@naver.com>
2023-09-23 22:43:42 +0900

    (#28) test: add util 3

--- seunggabi_core_python/util/obj_util.py
+++ seunggabi_core_python/util/obj_util.py
@@ -17,11 +17,3 @@ def safe_get(
         return default if obj is None else obj
     except:
         return default
-
-
-if __name__ == "__main__":
-    obj = {"a": 1, "z": {"b": 0}}
-    print(safe_get(obj, "a"))
-    print(safe_get(obj, "b"))
-    print(safe_get(obj, "b", 3))
-    print(safe_get(obj, "z.b"))
