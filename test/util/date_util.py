0020b213e21c122e270f8d798b6e668f2673f5d9
seunggabi <seunggabi@naver.com>
2023-09-23 22:36:33 +0900

    (#28) test: add util

---
+++ test/util/date_util.py
@@ -0,0 +1,33 @@
+import time
+
+from util.date_util import today, isoformat, timestamp
+from datetime import datetime
+
+
+# 테스트 케이스: today 함수 테스트
+def test_today_default_format():
+    expected_format = "%Y%m%d"
+    actual = today()
+    current_datetime = datetime.now()
+    expected = current_datetime.strftime(expected_format)
+    assert actual == expected
+
+
+def test_today_custom_format():
+    custom_format = "%Y-%m-%d"
+    actual = today(fmt=custom_format)
+    current_datetime = datetime.now()
+    expected = current_datetime.strftime(custom_format)
+    assert actual == expected
+
+
+def test_isoformat():
+    expected = datetime.now().isoformat().split(".")[0]
+    actual = isoformat().split(".")[0]
+    assert actual == expected
+
+
+def test_timestamp():
+    expected = int(float(time.time()))
+    actual = int(float(timestamp()))
+    assert actual == expected
---
+++ test/util/json_util.py
@@ -0,0 +1,32 @@
+from util.json_util import *
+
+
+def test_to_dict_valid_json():
+    json_str = '{"name": "John", "age": 30, "city": "New York"}'
+    result = to_dict(json_str)
+    expected = {"name": "John", "age": 30, "city": "New York"}
+    assert result == expected
+
+
+def test_pretty_dict():
+    input_dict = {"name": "John", "age": 30, "city": "New York"}
+    result = pretty(input_dict)
+    expected = (
+        '{\n    "age": 30,\n    "city": "New York",\n    "name": "John"\n}'
+    )
+    assert result == expected
+
+
+def test_pretty_json_str():
+    json_str = '{"name": "John", "age": 30, "city": "New York"}'
+    result = pretty(json_str)
+    expected = (
+        '{\n    "age": 30,\n    "city": "New York",\n    "name": "John"\n}'
+    )
+    assert result == expected
+
+
+def test_pretty_invalid_input():
+    invalid_input = "This is not JSON"
+    result = pretty(invalid_input)
+    assert result == invalid_input
