c6872cec15220216e92d3aefe0b9b8b415e1d4e2
seunggabi <seunggabi@naver.com>
2023-09-24 23:40:36 +0900

    (#28) test: bs_util

---
+++ test/util/bs_util.py
@@ -0,0 +1,27 @@
+import pytest
+from unittest.mock import patch
+from util.bs_util import crawl
+
+SAMPLE_HTML_CONTENT = """
+<html>
+  <body>
+    <div>
+      <a href="/ko/charts/#dominance-percentage">Sample Text</a>
+    </div>
+  </body>
+</html>
+"""
+
+URL = "https://coinmarketcap.com/ko/charts/"
+OBJ = {"href": "/ko/charts/#dominance-percentage"}
+
+
+@patch("util.request_util.get", return_value=SAMPLE_HTML_CONTENT)
+def test_crawl(mock_get):
+    result = crawl(url=URL, obj=OBJ)
+    expected = "Sample Text"
+    assert result == expected
+
+
+if __name__ == "__main__":
+    pytest.main()
