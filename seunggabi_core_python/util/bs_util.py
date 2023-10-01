f9306785837c04019d5b87e68131a8767c4a2fca
seunggabi <seunggabi@naver.com>
2023-10-01 20:14:47 +0900

    feat: soup

--- seunggabi_core_python/util/bs_util.py
+++ seunggabi_core_python/util/bs_util.py
@@ -2,8 +2,11 @@ from seunggabi_core_python.util import request_util
 from bs4 import BeautifulSoup
 
 
-def crawl(url: str = None, obj: dict = None, index: int = 0):
-    contents = request_util.get(url)
-    soup = BeautifulSoup(contents, "html.parser")
+def soup(url: str = None, obj: dict = None, index: int = 0):
+    return BeautifulSoup(request_util.get(url), "html.parser").findAll(**obj)[
+        index
+    ]
 
-    return soup.findAll(**obj)[index].get_text()
+
+def crawl(url: str = None, obj: dict = None, index: int = 0):
+    return soup(url, obj, index).get_text()
--- test/util/bs_util.py
+++ test/util/bs_util.py
@@ -1,6 +1,6 @@
 import pytest
 from unittest.mock import patch
-from util.bs_util import crawl
+from util.bs_util import crawl, soup
 
 SAMPLE_HTML_CONTENT = """
 <html>
@@ -24,6 +24,16 @@ def test_crawl(mock_get):
     result = crawl(url=URL, obj=OBJ)
     expected = "Sample Text"
     assert result == expected
+
+
+@patch(
+    "seunggabi_core_python.util.request_util.get",
+    return_value=SAMPLE_HTML_CONTENT,
+)
+def test_soup(mock_get):
+    result = soup(url=URL, obj=OBJ)
+    expected = "Sample Text"
+    assert result.get_text() == expected
 
 
 if __name__ == "__main__":
