c1413e91cd485b87f853f82b37f408e0145229da
seunggabi <seunggabi@naver.com>
2023-10-05 20:33:15 +0900

    (#31) feat: request_util.post

--- seunggabi_core_python/util/request_util.py
+++ seunggabi_core_python/util/request_util.py
@@ -3,6 +3,17 @@ import requests
 from typing import Union, Any
 from seunggabi_core_python.exception.not_ok import NotOk
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
+}
+
+
+def ok(response):
+    try:
+        return response.json()
+    except:
+        return response.text
+
 
 def get(
     url: str,
@@ -11,9 +22,7 @@ def get(
     headers=None,
 ) -> Any:
     params = params or {}
-    headers = headers or {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
-    }
+    headers = headers or HEADERS
 
     response = requests.get(
         url,
@@ -22,13 +31,60 @@ def get(
     )
 
     if response.status_code == 200:
-        try:
-            return response.json()
-        except:
-            return response.text
-    else:
-        retry -= 1
-        if retry <= 0:
-            raise NotOk(response.text)
+        return ok(response)
 
-        return get(url, params, retry)
+    retry -= 1
+    if retry <= 0:
+        raise NotOk(response.text)
+
+    return get(url, params, retry, headers)
+
+
+def post_data(
+    url: str,
+    data: Union[dict, None] = None,
+    retry: int = 3,
+    headers=None,
+) -> Any:
+    data = data or {}
+    headers = headers or HEADERS
+
+    response = requests.post(
+        url,
+        data=data,
+        headers=headers,
+    )
+
+    if response.status_code == 200:
+        return ok(response)
+
+    retry -= 1
+    if retry <= 0:
+        raise NotOk(response.text)
+
+    return post_data(url, data, retry, headers)
+
+
+def post_json(
+    url: str,
+    json: Union[dict, None] = None,
+    retry: int = 3,
+    headers=None,
+) -> Any:
+    json = json or {}
+    headers = headers or HEADERS
+
+    response = requests.post(
+        url,
+        json=json,
+        headers=headers,
+    )
+
+    if response.status_code == 200:
+        return ok(response)
+
+    retry -= 1
+    if retry <= 0:
+        raise NotOk(response.text)
+
+    return post_json(url, json, retry, headers)
